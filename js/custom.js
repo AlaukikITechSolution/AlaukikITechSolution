6438bd606f21e8db55bf1ffb158e44be8636bb67
Kishan Dasondhi <40437641+MaclaurinYudhisthira@users.noreply.github.com>
2021-02-21 22:15:00 +0530

    Code Files

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+	"use strict";
+
+	// Page loading animation
+	$(window).on('load', function () {
+		if ($('.cover').length) {
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function () {
+			setTimeout(function () {
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
+
+})(window.jQuery);
+
+document.getElementById('year').innerHTML=new Date().getFullYear()
